8fb430a21af33cd0f4eb635cbc169878aa86b2c4
Mei <meilani.eyre@gmail.com>
2018-01-18 15:33:34 -0800

    Did the bare minimum to make things work

--- lucky-number-model/Controllers/LuckyNumberController.cs
+++ lucky-number-model/Controllers/LuckyNumberController.cs
@@ -9,7 +9,7 @@ namespace lucky_number_model.Controllers
 {
     public class LuckyNumberController : Controller
     {
-        static int _starting_balance = 4; // Initial bank balance
+        static int _starting_balance = 5; // Initial bank balance
 
         [HttpGet]
         public ActionResult Spin() // runs the first time the form is loaded
@@ -18,10 +18,13 @@ namespace lucky_number_model.Controllers
             LuckyNumber myLuck = new LuckyNumber
             {
                 Number = 6,
-                Balance = _starting_balance
-            };
-            // TODO: Initialize the spinner fields for the ViewBag to zero
-
+                Balance = _starting_balance,
+                GameMessage = ""
+        };
+            // TODONE: Initialize the spinner fields for the ViewBag to zero
+            ViewBag.A = 0;
+            ViewBag.B = 0;
+            ViewBag.C = 0;
             
             // Pass the Model to the View
             return View(myLuck);
@@ -33,31 +36,33 @@ namespace lucky_number_model.Controllers
             // GAME PLAY : If a spin would cause a negative balance, send the view a "Game Over" message and reset Balance
             if (lucky.Balance <= 0)
             {
-                ViewBag.Error = "GAME OVER: Spin to try again";
+                lucky.GameMessage = "GAME OVER: Spin to try again";
                 lucky.Balance = _starting_balance;
 
                 // Pass the Model to the View (this ends the method)
                 return View(lucky);
             }
 
-            // TODO: Charge the cost of a spin (subtract 1 from the Balance)
+            // TODONE: Charge the cost of a spin (subtract 1 from the Balance)
+            lucky.Balance -= 1;
 
 
-
-            // TODO: Assign a random value between 1 and 9 to three local variables, a, b, and c
+            // TODONE: Assign a random value between 1 and 9 to three local variables, a, b, and c
             int a=0, b=0, c=0;
+            var randomNumber = new System.Random();
+            a = randomNumber.Next(0, 10);
+            b = randomNumber.Next(0, 10);
+            c = randomNumber.Next(0, 10);
 
 
-
-            // TODO: Assign the ViewBag variables these local variable values
-
-
-
+            // TODONE: Assign the ViewBag variables these local variable values
+            ViewBag.a = a;
+            ViewBag.b = b;
+            ViewBag.c = c;
 
             //Check for a winner, update Balance and the isWinner flag
             if (a== lucky.Number || b == lucky.Number || c == lucky.Number)
             {
-                lucky.Balance += 2;
                 lucky.isWinner = true;
             }
 
