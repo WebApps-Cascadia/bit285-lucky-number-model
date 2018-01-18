bcccac2392d0edb8f5cf6cd3f06916ad2d91eb92
James Hayes <jchayes86@gmail.com>
2018-01-18 15:40:28 -0800

    my changes

--- lucky-number-model/Models/LuckyNumber.cs
+++ lucky-number-model/Models/LuckyNumber.cs
@@ -8,14 +8,61 @@ namespace lucky_number_model.Models
 {
     public class LuckyNumber
     {
+        private decimal _balance;
+        private Boolean _iswinner;
+        private int[] _spinner = new int[3];
+
+        private Random _rnd = new Random();
+
         [Required]
         [Display(Name = "Lucky Number")]
         [Range(1, 9, ErrorMessage = "Number must be between 1 and 9")]
         public int Number { get; set; } // the user's choice of a lucky number
 
+        // the balance for this game
         [Required]
-        public decimal Balance { get; set; } // the balance for this game
+        public decimal Balance
+        {
+            get
+            {
+                if (isWinner) { _balance = _balance + 2; }
+                return _balance;
+            }
+            set
+            {
+                _balance = value;
+            }
+        }
 
         public Boolean isWinner { get; set; } // a flag for when the lucky number matches the spin
+
+        public string GameMessage
+        {
+            get
+            {
+                if (Balance <= 0) { return "GAME OVER: Spin to try again!"; }
+                return "";
+            }
+            set
+            {
+
+            }
+        }
+
+        //public int[] spinner
+        //{
+        //    get
+        //    {
+        //        for (int i = 0; i <= 2; i++)
+        //        {
+        //            spinner[i] = _rnd.Next(1, 10);
+        //        }
+        //        return spinner;
+        //    }
+        //    set
+        //    {
+
+        //    }
+        //}
     }
 }
