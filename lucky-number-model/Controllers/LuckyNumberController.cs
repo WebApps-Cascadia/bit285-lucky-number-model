34dcc31ea6c821cfaad75c83760d49d35de82277
Alex <usalex93@gmail.com>
2018-01-18 22:47:14 -0800

    Changed Lucky 7. Have problems to move balance logic into LuckyNumber.cs. Can you take a look at my code please and tell me what I did wrong?

--- lucky-number-model/Controllers/LuckyNumberController.cs
+++ lucky-number-model/Controllers/LuckyNumberController.cs
@@ -10,10 +10,10 @@ namespace lucky_number_model.Controllers
     public class LuckyNumberController : Controller
     {
         static int _starting_balance = 4; // Initial bank balance
-
         [HttpGet]
         public ActionResult Spin() // runs the first time the form is loaded
         {
+
             //Build a default Model with some initial values 
             LuckyNumber myLuck = new LuckyNumber
             {
@@ -21,8 +21,10 @@ namespace lucky_number_model.Controllers
                 Balance = _starting_balance
             };
             // TODO: Initialize the spinner fields for the ViewBag to zero
+            //ViewBag.A = 0;
+            //ViewBag.B = 0;
+            //ViewBag.C = 0;
 
-            
             // Pass the Model to the View
             return View(myLuck);
         }
@@ -30,22 +32,41 @@ namespace lucky_number_model.Controllers
         [HttpPost]
         public ActionResult Spin(LuckyNumber lucky) //The Model is passed in with values from the form submission
         {
-            // GAME PLAY : If a spin would cause a negative balance, send the view a "Game Over" message and reset Balance
-            if (lucky.Balance <= 0)
+
+            if (lucky.Spinner[0] == lucky.Number || lucky.Spinner[1] == lucky.Number || lucky.Spinner[2] == lucky.Number)
             {
-                ViewBag.Error = "GAME OVER: Spin to try again";
-                lucky.Balance = _starting_balance;
+                lucky.Balance += 2; // move this to the smart balance property
+                lucky.isWinner = true;
+            }
+            lucky.Balance -= 1;
+
+
+            // GAME PLAY : If a spin would cause a negative balance, send the view a "Game Over" message and reset Balance
+            if (lucky.Balance < 0)
+            {
+
+                lucky.Balance = 4;
 
                 // Pass the Model to the View (this ends the method)
                 return View(lucky);
             }
 
-            // TODO: Charge the cost of a spin (subtract 1 from the Balance)
 
+
+            // TODO: Charge the cost of a spin (subtract 1 from the Balance)
+            //lucky.Balance = lucky.Balance - 1;
 
 
             // TODO: Assign a random value between 1 and 9 to three local variables, a, b, and c
-            int a=0, b=0, c=0;
+            //var rnd = new System.Random();
+
+            //var a = rnd.Next(1, 10);
+            //var b = rnd.Next(1, 10);
+            //var c = rnd.Next(1, 10);
+
+            //ViewBag.A = a;
+            //ViewBag.B = b;
+            //ViewBag.C = c;
 
 
 
@@ -54,12 +75,12 @@ namespace lucky_number_model.Controllers
 
 
 
-            //Check for a winner, update Balance and the isWinner flag
-            if (a== lucky.Number || b == lucky.Number || c == lucky.Number)
-            {
-                lucky.Balance += 2;
-                lucky.isWinner = true;
-            }
+            // IN MODEL Check for a winner, update Balance and the isWinner flag
+            //if (a == lucky.Number || b == lucky.Number || c == lucky.Number)
+            //{
+            //    lucky.Balance += 2; // move this to the smart balance property
+            //    lucky.isWinner = true;
+            //}
 
             // Pass the Model to the View
             return View(lucky);
