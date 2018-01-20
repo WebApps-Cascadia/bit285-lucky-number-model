a17f2f0dc33e4c8dab9a40be05399463f6020a0c
BenC <ben@ceterisdev.com>
2018-01-20 11:31:42 -0800

    fixed up my errors

--- lucky-number-model/Controllers/LuckyNumberController.cs
+++ lucky-number-model/Controllers/LuckyNumberController.cs
@@ -21,8 +21,10 @@ namespace lucky_number_model.Controllers
                 Balance = _starting_balance
             };
             // TODO: Initialize the spinner fields for the ViewBag to zero
+            ViewBag.A = 0;
+            ViewBag.B = 0;
+            ViewBag.C = 0;
 
-            
             // Pass the Model to the View
             return View(myLuck);
         }
@@ -34,7 +36,6 @@ namespace lucky_number_model.Controllers
             if (lucky.Balance <= 0)
             {
                 ViewBag.Error = "GAME OVER: Spin to try again";
-                //ViewBag.Error = "GAME OVER: Spin to try again";
                 lucky.Balance = _starting_balance;
 
                 // Pass the Model to the View (this ends the method)
@@ -44,8 +45,6 @@ namespace lucky_number_model.Controllers
             // TODO: Charge the cost of a spin (subtract 1 from the Balance)
             lucky.Balance -= 1;
 
-
-
             // TODO: Assign a random value between 1 and 9 to three local variables, a, b, and c
             int a=0, b=0, c=0;
             Random rnd = new Random();
@@ -54,7 +53,6 @@ namespace lucky_number_model.Controllers
             c = rnd.Next(0, 10);
 
             // TODO: Assign the ViewBag variables these local variable values
-
             ViewBag.A = a;
             ViewBag.B = b;
             ViewBag.C = c;
